dc4987f8af7b3c04ce4f7f19a6cf9d557e854c9f
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2025-06-01 12:40:38 +0200

    possible fis japanese text cut off in theme selector

--- app/src/main/java/com/dergoogler/mmrl/wx/ui/screens/settings/appTheme/AppThemeScreen.kt
+++ app/src/main/java/com/dergoogler/mmrl/wx/ui/screens/settings/appTheme/AppThemeScreen.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
@@ -44,7 +46,8 @@ fun AppThemeScreen() {
     ) {
         Column(
             modifier = Modifier
-                .padding(it),
+                .padding(it)
+                .verticalScroll(rememberScrollState()),
         ) {
             Column(
                 modifier = Modifier
